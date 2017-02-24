3c446c4db63c32bc26de5db674b2745e81746ca6
Alin Voinea <alin@eaudeweb.ro>
2017-02-24 18:33:38 +0200

    Refs #80360 - Speedup startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -vp /data/www-static-resources /data/eea.controlpanel
-chown -R plone:plone /data/www-static-resources /data/eea.controlpanel
+chown plone:plone /data/www-static-resources /data/eea.controlpanel
 
 if [ ! -z "$TRACEVIEW" ]; then
     /usr/sbin/traceview-config $TRACEVIEW
