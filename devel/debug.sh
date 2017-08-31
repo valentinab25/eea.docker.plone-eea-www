6a862dd37308137e4752d2286c2197d7abf513bd
Alin Voinea <alin.voinea@eaudeweb.ro>
2017-08-31 16:02:19 +0300

    Refs #87600 - Make debug.sh command mode verbose

--- devel/debug.sh
+++ devel/debug.sh
@@ -2,6 +2,15 @@
 
 bin/develop rb
 
+echo "============================================================="
+echo "All set. Now you can dive into container and start debugging:"
+echo "                                                             "
+echo "    $ docker exec -it <container_name_or_id> bash            "
+echo "    $ ps aux                                                 "
+echo "    $ bin/instance fg                                        "
+echo "                                                             "
+echo "============================================================="
+
 if [ -z "$1" ]; then
   exec cat
 fi
