3205df81af68ff336339a05ac82f3a74830382f8
Alin Voinea <alin.voinea@eaudeweb.ro>
2017-09-05 19:23:14 +0300

    Refs #87909 - Auto-find tests to exclude

--- devel/debug.sh
+++ devel/debug.sh
@@ -17,13 +17,23 @@ fi
 
 if [ "$1" == "tests" ]; then
  for i in $(ls src); do
+
+   AUTO_EXCLUDE="$(cat bin/test | grep src/$i)"
+   if [ -z "$AUTO_EXCLUDE" ]; then
+       echo "============================================================="
+       echo "Auto: Skipping tests for: $i                                 "
+       continue
+   fi
+
    if [ ! -z "$EXCLUDE" ]; then
      if [[ $EXCLUDE == *"$i"* ]]; then
        echo "============================================================="
-       echo "Skipping tests for: $i                                       "
+       echo "Manual: Skipping tests for: $i                               "
        continue
      fi
    fi
+
+
 
    echo "============================================================="
    echo "Running tests for:                                           "
@@ -31,7 +41,7 @@ if [ "$1" == "tests" ]; then
    echo "    $i                                                       "
    echo "                                                             "
 
-   ./bin/test -v -vv -s $i
+   ./bin/test --test-path /plone/instance/src/$i -v -vv -s $i
   done
 fi
 
